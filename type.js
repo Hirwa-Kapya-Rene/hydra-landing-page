ae48dee8d1ffa1a6dc51c9ab6e5cf5c0fa0148dd
Hirwa Rene <hirwarene6@gmail.com>
2025-04-01 11:15:59 +0200

    added attributs to User

--- type.js
+++ type.js
@@ -1,3 +1,13 @@
 class User{
-  
+  nom : string;
+  prenom : string;
+  age : number;
+  constructor(nom : string, prenom : string, age : number){
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+  }
+  getNom(){
+    return this.nom;
+  } 
 }
